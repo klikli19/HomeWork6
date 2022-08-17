18cce7fd6f1f8d4c22e3b16fbe8821ebb084f2cc
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-17 22:00:18 +0300

    все сделала, кроме задачи 7

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         int sum = 0;
-        for (int element: arr ){
+        for (int element : arr) {
             sum += element;
         }
         System.out.println("Сумма затрат за месяц равна " + sum + " рублей.");
@@ -14,7 +17,7 @@ public class Main {
         int maxSpending = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxSpending) {
-                maxSpending = arr [i];
+                maxSpending = arr[i];
             }
         }
         System.out.println("Максимальная сумма трат за день составила " + maxSpending + " рублей");
@@ -34,13 +37,70 @@ public class Main {
         System.out.println();
 
         System.out.println("Задание 4");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i =reverseFullName.length -1; i >=0; i--) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Задание 5");
+        int size = 3;
+        int[][] twoDimArray = new int[size][size];
+        for (int i = 0; i < twoDimArray.length; i++) {
+            twoDimArray[i][i] = 1;
+            twoDimArray[i][size - 1 - i] = 1;
+            twoDimArray[i][i] = 1;
+        }
+        for (int[] row : twoDimArray) {
+            for (int column : row) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Задание 6");
+        int[] days = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(days));
+        int[] arr1 = Arrays.copyOf(days, days.length);
+        Arrays.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+        // или
+        int index = days.length;
+        int [] array = new  int[index];
+        for (int i = 0; i < days.length; i++) {
+            index = days.length - 1 - i;
+            array[index] = days[i];
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println();
+
+        System.out.println("Задание 8");
+        int[] box = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        for (int i : box) {
+            for (int m : box) {
+                if (i + m == -2) {
+                    System.out.println(i + ", " + m);
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println("Задание 9");
+        for (int i : box) {
+            for (int m : box) {
+                if (i + m == -2) {
+                    System.out.println(i + ", " + m);
+                }
+            }
         }
     }
 
+
+
     static int[] arr = generateRandomArray();
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -50,5 +110,16 @@ public class Main {
         return arr;
     }
 
+    public static void bubbleSort(int[] arrs) {
+        for (int i = arrs.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arrs[j] > arrs[j + 1]) {
+                    int temp = arrs[j];
+                    arrs[j] = arrs[j + 1];
+                    arrs[j + 1] = temp;
+                }
+            }
+        }
 
+    }
 }
